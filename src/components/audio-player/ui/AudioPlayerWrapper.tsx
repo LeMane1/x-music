8ec2d714c69422960d9de7aed154585d9f523994
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-17 00:40:48 +0300

    updated AudioPlayer view

--- src/components/audio-player/ui/AudioPlayerWrapper.tsx
+++ src/components/audio-player/ui/AudioPlayerWrapper.tsx
@@ -1,28 +1,32 @@
 'use client'
 
 import {Box, Container} from "@mui/material";
+import {useMediaQuery} from "@mui/system";
+import theme from "@/styles/theme";
 
 interface IAudioPlayerWrapperProps {
   children?: React.ReactNode;
 }
 
 export default function AudioPlayerWrapper({children}: IAudioPlayerWrapperProps) {
+  const isLgSize = useMediaQuery(theme.breakpoints.up("lg"));
+  
   return (
     <Box sx={{
       position: 'fixed',
-      bottom: 0,
+      bottom: isLgSize ? 16 : 0,
       left: 0,
       width: '100%',
       height: 60,
       display: 'flex',
       justifyContent: 'center',
-      backgroundColor: "rgba(115,115,115,0.2)",
-      backdropFilter: 'blur(20px)',
     }}>
       <Container
         maxWidth='lg'
         sx={{
-          
+          borderRadius: isLgSize ? 2 : 0,
+          backgroundColor: "rgba(115,115,115,0.2)",
+          backdropFilter: 'blur(20px)',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
