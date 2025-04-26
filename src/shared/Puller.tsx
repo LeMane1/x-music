dc89c853cba62febe75bd2cdbfaede4c5429af39
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-26 11:29:32 +0300

    added Puller shared comp

---
+++ src/shared/Puller.tsx
@@ -0,0 +1,12 @@
+import {Box} from "@mui/material";
+
+export default function Puller(){
+  return (
+    <Box sx={{
+      width: 40,
+      height: 6,
+      borderRadius: 4,
+      backgroundColor: 'rgb(122,122,122)',
+    }}/>
+  )
+}
